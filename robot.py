9c9eededf96922170f00f21c02687b81551f79c1
HunterBowie <73845280+HunterBowie@users.noreply.github.com>
2022-09-22 09:48:26 -0700

    added tasks for testing and deploying code

--- robot.py
+++ robot.py
@@ -1,21 +1,24 @@
 import wpilib
 import wpilib.drive
-
+import ctre
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
-        self.front_left_motor = wpilib.PWMSparkMax(1)
-        self.front_right_motor = wpilib.PWMSparkMax(2)
-        self.back_left_motor = wpilib.PWMSparkMax(0)
-        self.back_right_motor = wpilib.PWMSparkMax(3)
+        self.back_left = ctre.WPI_VictorSPX(9)
+        self.front_right = ctre.WPI_VictorSPX(10)
+        self.back_right = ctre.WPI_VictorSPX(8)
+        self.front_left = ctre.WPI_VictorSPX(7)
 
-        self.left_side = wpilib.MotorControllerGroup(self.front_left_motor, self.back_left_motor)
-        self.right_side = wpilib.MotorControllerGroup(self.front_right_motor, self.back_right_motor)
+        left_side = wpilib.MotorControllerGroup(self.front_left, self.back_left)
+        right_side = wpilib.MotorControllerGroup(self.front_right, self.back_right)
+        self.myRobot = wpilib.drive.DifferentialDrive(left_side, right_side)
 
-        self.drive = wpilib.drive.DifferentialDrive(self.left_side, self.right_side)
         self.stick = wpilib.Joystick(0)
 
     def teleopPeriodic(self):
-        self.drive.arcadeDrive(-self.stick.getRawAxis(0), self.stick.getRawAxis(1))
+        self.myRobot.arcadeDrive(
+            self.stick.getRawAxis(0), -self.stick.getRawAxis(1)
+        )
 
+    
 wpilib.run(Robot)
